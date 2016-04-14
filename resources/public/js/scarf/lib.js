4232d39af88694412fcc4a2a82ffd4d0e34a4382
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-04-14 22:46:37 +0200

    Play around with Raphael

--- resources/public/js/scarf/lib.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.8.40 {}
-goog.provide('scarf.lib');
-goog.require('cljs.core');
-
-//# sourceMappingURL=lib.js.map
---
+++ src/scarf/views.cljs
@@ -0,0 +1,4 @@
+(ns scarf.views
+  (:require [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]))
