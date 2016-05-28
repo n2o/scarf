be7fa4796d22c2610521eec6552f60b7b5f63aa6
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-05-28 17:22:13 +0200

    Tidy up, show current selection

--- src/scarf/views.cljs
+++ src/scarf/views.cljs
@@ -12,31 +12,6 @@
 (def width "276px")
 (def height "138px")
 
-(defui ^:once Scarf
-  static om/IQuery
-  (query [this]
-    [:scarf])
-  Object
-  (render [this]
-    (dom/div nil
-             (dom/svg #js {:enable-background "new 0 0 276 138"
-                           :viewBow           "0 0 276 128"
-                           :height            "138px"
-                           :width             "276px"
-                           :x                 "0"
-                           :y                 "0"}
-                      (dom/g nil
-                             (dom/polygon #js {:onClick #(lib/update-color! :color1)
-                                               :fill    (get-in @lib/app-state [:scarf :color1])
-                                               :points  "266.118,0 138.001,127.452 9.882,0 0,0 138.001,138 276,0"})
-                             (dom/polygon #js {:onClick #(lib/update-color! :color2)
-                                               :fill    (get-in @lib/app-state [:scarf :color2])
-                                               :points  "266.118,0 246.666,0 138.001,108.833 29.333,0 9.882,0 138.001,127.452"})
-                             (dom/polygon #js {:onClick #(lib/update-color! :color1)
-                                               :fill    (get-in @lib/app-state [:scarf :color1])
-                                               :points  "246.666,0 29.333,0 138.001,108.833"}))))))
-(def scarf (om/factory Scarf {}))
-
 (defui ^:once ColorBlock
   Object
   (render [this]
@@ -55,10 +30,40 @@
              (let [colors (lib/get-colors)]
                (apply dom/ul #js {:id "colors"}
                       (map #(color-block {:color %}) colors))))))
+
 (defn colors
   "Create color palette"
   ([data] (om/factory Colors data))
   ([] (colors {})))
+
+(defui ^:once Scarf
+  static om/IQuery
+  (query [this]
+    [:scarf :user])
+  Object
+  (render [this]
+    (dom/div nil
+             (dom/div nil
+                      (dom/span nil
+                                "Current selection:"
+                                (color-block {:color [(lib/get-selected-color) "Aktuelle Auswahl"]})))
+             (dom/svg #js {:enable-background "new 0 0 276 138"
+                           :viewBow           "0 0 276 128"
+                           :height            "138px"
+                           :width             "276px"
+                           :x                 "0"
+                           :y                 "0"}
+                      (dom/g nil
+                             (dom/polygon #js {:onClick #(lib/update-color! :color1)
+                                               :fill    (get-in @lib/app-state [:scarf :color1])
+                                               :points  "266.118,0 138.001,127.452 9.882,0 0,0 138.001,138 276,0"})
+                             (dom/polygon #js {:onClick #(lib/update-color! :color2)
+                                               :fill    (get-in @lib/app-state [:scarf :color2])
+                                               :points  "266.118,0 246.666,0 138.001,108.833 29.333,0 9.882,0 138.001,127.452"})
+                             (dom/polygon #js {:onClick #(lib/update-color! :color1)
+                                               :fill    (get-in @lib/app-state [:scarf :color1])
+                                               :points  "246.666,0 29.333,0 138.001,108.833"}))))))
+(def scarf (om/factory Scarf {}))
 
 #_(defui Main
     Object
