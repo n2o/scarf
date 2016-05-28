c91b82335c78ec108635d6e1a2f2c07130b3c24a
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-05-28 17:30:18 +0200

    Add more elegance

--- src/scarf/views.cljs
+++ src/scarf/views.cljs
@@ -43,10 +43,6 @@
   Object
   (render [this]
     (dom/div nil
-             (dom/div nil
-                      (dom/span nil
-                                "Current selection:"
-                                (color-block {:color [(lib/get-selected-color) "Aktuelle Auswahl"]})))
              (dom/svg #js {:enable-background "new 0 0 276 138"
                            :viewBow           "0 0 276 128"
                            :height            "138px"
@@ -62,7 +58,10 @@
                                                :points  "266.118,0 246.666,0 138.001,108.833 29.333,0 9.882,0 138.001,127.452"})
                              (dom/polygon #js {:onClick #(lib/update-color! :color1)
                                                :fill    (get-in @lib/app-state [:scarf :color1])
-                                               :points  "246.666,0 29.333,0 138.001,108.833"}))))))
+                                               :points  "246.666,0 29.333,0 138.001,108.833"})))
+             (dom/div nil
+                      (dom/span nil "Current selection:")
+                      (color-block {:color [(lib/get-selected-color) "Aktuelle Auswahl"]})))))
 (def scarf (om/factory Scarf {}))
 
 #_(defui Main
