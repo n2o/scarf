ccaac159b8b247ba46766fb9116747e6db94ca48
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-05-31 17:51:34 +0200

    Change data structure of colors

--- src/scarf/lib.cljs
+++ src/scarf/lib.cljs
@@ -2,42 +2,69 @@
   (:require [om.next :as om :refer-macros [defui]]))
 
 (defonce app-state
-  (atom
-    {:scarf     {:color1 "white"
-                 :color2 "white"}
-     :colors    [["#efe406" "Hellgelb"]
-                 ["#f3e747" "Zitrone"]
-                 ["#c98200" "Orange"]
-                 ["#b92f1f" "Scharlachrot"]
-                 ["#b92f1f" "Dunkelrot"]
-                 ["#480d2c" "Kastanienbraun"]
-                 ["#5c068c" "Lila"]
-                 ["#c98f47" "Gold"]
-                 ["#826e57" "Kaki"]
-                 ["#422310" "Schokolade"]
-                 ["#7b0065" "Traube"]
-                 ["#1a6a30" "Smaragdgrün"]
-                 ["#1a6a30" "Piniengrün"]
-                 ["#143422" "Scoutgrün"]
-                 ["#afc3d5" "Himmelfarben"]
-                 ["#21145c" "Königsblau"]
-                 ["#152442" "Marineblau"]
-                 ["#f9f9f9" "Weiß"]
-                 ["#7e7f84" "Grau"]
-                 ["#000000" "Schwarz"]
-                 ["#cd6888" "Pink"]
-                 ["#b9cfe4" "Blaugrau"]
-                 ["#44bce3" "Türkis"]
-                 ["#ff5e00" "Orangerot"]
-                 ["#c2bcec" "Fliederfarben"]
-                 ["#e6ff15" "Tageslicht Gelb"]]
-     :user      {:selected-color ""}}))
+         (atom
+           {:scarf  {:color1 "white"
+                     :color2 "white"}
+            :colors [{:rgb  "#efe406"
+                      :name "Hellgelb"}
+                     {:rgb  "#f3e747"
+                      :name "Zitrone"}
+                     {:rgb  "#c98200"
+                      :name "Orange"}
+                     {:rgb  "#b92f1f"
+                      :name "Scharlachrot"}
+                     {:rgb  "#b92f1f"
+                      :name "Dunkelrot"}
+                     {:rgb  "#480d2c"
+                      :name "Kastanienbraun"}
+                     {:rgb  "#5c068c"
+                      :name "Lila"}
+                     {:rgb  "#c98f47"
+                      :name "Gold"}
+                     {:rgb  "#826e57"
+                      :name "Kaki"}
+                     {:rgb  "#422310"
+                      :name "Schokolade"}
+                     {:rgb  "#7b0065"
+                      :name "Traube"}
+                     {:rgb  "#1a6a30"
+                      :name "Smaragdgrün"}
+                     {:rgb  "#1a6a30"
+                      :name "Piniengrün"}
+                     {:rgb  "#143422"
+                      :name "Scoutgrün"}
+                     {:rgb  "#afc3d5"
+                      :name "Himmelfarben"}
+                     {:rgb  "#21145c"
+                      :name "Königsblau"}
+                     {:rgb  "#152442"
+                      :name "Marineblau"}
+                     {:rgb  "#f9f9f9"
+                      :name "Weiß"}
+                     {:rgb  "#7e7f84"
+                      :name "Grau"}
+                     {:rgb  "#000000"
+                      :name "Schwarz"}
+                     {:rgb  "#cd6888"
+                      :name "Pink"}
+                     {:rgb  "#b9cfe4"
+                      :name "Blaugrau"}
+                     {:rgb  "#44bce3"
+                      :name "Türkis"}
+                     {:rgb  "#ff5e00"
+                      :name "Orangerot"}
+                     {:rgb  "#c2bcec"
+                      :name "Fliederfarben"}
+                     {:rgb  "#e6ff15"
+                      :name "Tageslicht Gelb"}]
+            :user   {:selected-color ""}}))
 
 ;;;; Auxiliary
 (defn get-selected-color
   "Return the user's current selection."
   []
   (get-in @app-state [:user :selected-color]))
+
 
 ;;;; Functions concerning the reconciler
 (defmulti read (fn [env key params] key))
@@ -52,7 +79,8 @@
 (defmulti mutate om/dispatch)
 
 (defmethod mutate 'color/temp
-  [{:keys [state]} _ {:keys [name color]}]
+  [{:keys [state]} _ {:keys [color]}]
+  (println color)
   {:action (fn [] (swap! state update-in [:user :selected-color] (fn [] color)))})
 
 (defmethod mutate 'color/set
@@ -61,11 +89,12 @@
     {:action (fn [] (swap! state update-in [:scarf field] (fn [] color)))}))
 
 (defonce reconciler
-  (om/reconciler
-    {:state app-state
-     :parser (om/parser {:read read :mutate mutate})}))
+         (om/reconciler
+           {:state  app-state
+            :parser (om/parser {:read read :mutate mutate})}))
 
-;;;; Setting Colors
+
+;;;; Colors
 (defn save-selected-color!
   "Store the selected color in the app-state."
   [color]
@@ -76,7 +105,6 @@
   [field]
   (om/transact! reconciler `[(color/set {:field ~field})]))
 
-;;;; Getter
 (defn get-colors
   "Return list with all available colors."
   []
