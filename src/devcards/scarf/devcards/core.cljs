edff34f94b1012914d48f938dd3cf228743ae0ba
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-05-31 17:51:42 +0200

    Apply new data structure to views

--- src/devcards/scarf/devcards/core.cljs
+++ src/devcards/scarf/devcards/core.cljs
@@ -1,27 +1,17 @@
 (ns scarf.devcards.core
-  (:require [goog.dom :as gdom]
-            [devcards.core :as dc :refer-macros [defcard deftest]]
-            [devcards-om-next.core :as don :refer-macros [om-next-root defcard-om-next]]
+  (:require [devcards.core :refer-macros [defcard deftest]]
+            [devcards-om-next.core :refer-macros [om-next-root defcard-om-next]]
             [cljs.test :refer-macros [is testing]]
-            [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]
             [scarf.lib :as lib]
             [scarf.views :as views]))
 
 (enable-console-print!)
 
-(def width "276px")
-(def height "138px")
-
+;;;; Init
 (defcard-om-next scarf-om-next
                  "## Main Scarf Component"
                  views/Scarf
                  lib/reconciler)
-
-#_(defcard-om-next colors-card
-                   "## Choose your color"
-                   Colors
-                   lib/reconciler)
 
 (defcard colors
          ""
--- src/scarf/views.cljs
+++ src/scarf/views.cljs
@@ -3,6 +3,7 @@
             [cljs.test :refer-macros [is testing]]
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]
+            [scarf.extensions]
             [scarf.lib :as lib]))
 
 (enable-console-print!)
@@ -13,12 +14,12 @@
 (defui ^:once ColorBlock
   Object
   (render [this]
-    (let [[color name] (:color (om/props this))]
+    (let [color (om/props this)]
       (dom/div #js {:className "color-block-wrapper"
                     :onClick   #(lib/save-selected-color! color)
-                    :style     #js {:backgroundColor color}}
+                    :style     #js {:backgroundColor (:rgb color)}}
                (dom/div #js {:className "color-block"})
-               (dom/span #js {:className "tooltiptext"} name)))))
+               (dom/span #js {:className "tooltiptext"} (:name color))))))
 (def color-block (om/factory ColorBlock {}))
 
 (defui ^:once Colors
@@ -27,12 +28,10 @@
     (dom/div nil
              (let [colors (lib/get-colors)]
                (apply dom/ul #js {:id "colors"}
-                      (map #(color-block {:color %}) colors))))))
-
+                      (map #(color-block %) colors))))))
 (defn colors
   "Create color palette"
-  ([data] (om/factory Colors data))
-  ([] (colors {})))
+  [] (om/factory Colors {}))
 
 (defui ^:once Scarf
   static om/IQuery
@@ -49,17 +48,17 @@
                            :y                 "0"}
                       (dom/g nil
                              (dom/polygon #js {:onClick #(lib/update-color! :color1)
-                                               :fill    (get-in @lib/app-state [:scarf :color1])
+                                               :fill    (:rgb (get-in @lib/app-state [:scarf :color1]))
                                                :points  "266.118,0 138.001,127.452 9.882,0 0,0 138.001,138 276,0"})
                              (dom/polygon #js {:onClick #(lib/update-color! :color2)
-                                               :fill    (get-in @lib/app-state [:scarf :color2])
+                                               :fill    (:rgb (get-in @lib/app-state [:scarf :color2]))
                                                :points  "266.118,0 246.666,0 138.001,108.833 29.333,0 9.882,0 138.001,127.452"})
                              (dom/polygon #js {:onClick #(lib/update-color! :color1)
-                                               :fill    (get-in @lib/app-state [:scarf :color1])
+                                               :fill    (:rgb (get-in @lib/app-state [:scarf :color1]))
                                                :points  "246.666,0 29.333,0 138.001,108.833"})))
              (dom/div nil
                       (dom/span nil "Current selection:")
-                      (color-block {:color [(lib/get-selected-color) "Aktuelle Auswahl"]})))))
+                      (color-block (lib/get-selected-color))))))
 (def scarf (om/factory Scarf {}))
 
 #_(defui Main
