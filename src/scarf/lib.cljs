f1bd8cb298c92d5d20fa6aec8ee9a336561d3983
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-02-05 23:01:45 +0100

    Reactify color blocks

--- src/scarf/lib.cljs
+++ src/scarf/lib.cljs
@@ -1,63 +1,56 @@
 (ns scarf.lib
-  (:require [om.next :as om :refer-macros [defui]]
+  (:require [om.next :as om]
             [scarf.config :as config]))
 
 (defn num->currency
   [value]
   (.toLocaleString value "de-DE" #js {:style "currency" :currency "EUR"}))
 
-(def colors
-  "Define all available colors."
-  [{:rgb "#efe406", :name "Hellgelb", :price (num->currency (rand 5))}
-   {:rgb "#f3e747", :name "Zitrone", :price (num->currency (rand 5))}
-   {:rgb "#c98200", :name "Orange", :price (num->currency (rand 5))}
-   {:rgb "#b92f1f", :name "Scharlachrot", :price (num->currency (rand 5))}
-   {:rgb "#b92f1f", :name "Dunkelrot", :price (num->currency (rand 5))}
-   {:rgb "#480d2c", :name "Kastanienbraun", :price (num->currency (rand 5))}
-   {:rgb "#5c068c", :name "Lila", :price (num->currency (rand 5))}
-   {:rgb "#c98f47", :name "Gold", :price (num->currency (rand 5))}
-   {:rgb "#826e57", :name "Kaki", :price (num->currency (rand 5))}
-   {:rgb "#422310", :name "Schokolade", :price (num->currency (rand 5))}
-   {:rgb "#7b0065", :name "Traube", :price (num->currency (rand 5))}
-   {:rgb "#1a6a30", :name "Smaragdgrün", :price (num->currency (rand 5))}
-   {:rgb "#1a6a30", :name "Piniengrün", :price (num->currency (rand 5))}
-   {:rgb "#143422", :name "Scoutgrün", :price (num->currency (rand 5))}
-   {:rgb "#afc3d5", :name "Himmelfarben", :price (num->currency (rand 5))}
-   {:rgb "#21145c", :name "Königsblau", :price (num->currency (rand 5))}
-   {:rgb "#152442", :name "Marineblau", :price (num->currency (rand 5))}
-   {:rgb "#f9f9f9", :name "Weiß", :price (num->currency (rand 5))}
-   {:rgb "#7e7f84", :name "Grau", :price (num->currency (rand 5))}
-   {:rgb "#000000", :name "Schwarz", :price (num->currency (rand 5))}
-   {:rgb "#cd6888", :name "Pink", :price (num->currency (rand 5))}
-   {:rgb "#b9cfe4", :name "Blaugrau", :price (num->currency (rand 5))}
-   {:rgb "#44bce3", :name "Türkis", :price (num->currency (rand 5))}
-   {:rgb "#ff5e00", :name "Orangerot", :price (num->currency (rand 5))}
-   {:rgb "#c2bcec", :name "Fliederfarben", :price (num->currency (rand 5))}
-   {:rgb "#e6ff15", :name "Tageslicht Gelb", :price (num->currency (rand 5))}])
+(def app-state
+  {:scarf/color1 "white"
+   :scarf/color2 "white"
+   :color/items [{:rgb "#efe406", :name "Hellgelb", :price (num->currency (rand 5))}
+                 {:rgb "#f3e747", :name "Zitrone", :price (num->currency (rand 5))}
+                 {:rgb "#c98200", :name "Orange", :price (num->currency (rand 5))}
+                 {:rgb "#b92f1f", :name "Scharlachrot", :price (num->currency (rand 5))}
+                 {:rgb "#b92f1f", :name "Dunkelrot", :price (num->currency (rand 5))}
+                 {:rgb "#480d2c", :name "Kastanienbraun", :price (num->currency (rand 5))}
+                 {:rgb "#5c068c", :name "Lila", :price (num->currency (rand 5))}
+                 {:rgb "#c98f47", :name "Gold", :price (num->currency (rand 5))}
+                 {:rgb "#826e57", :name "Kaki", :price (num->currency (rand 5))}
+                 {:rgb "#422310", :name "Schokolade", :price (num->currency (rand 5))}
+                 {:rgb "#7b0065", :name "Traube", :price (num->currency (rand 5))}
+                 {:rgb "#1a6a30", :name "Smaragdgrün", :price (num->currency (rand 5))}
+                 {:rgb "#1a6a30", :name "Piniengrün", :price (num->currency (rand 5))}
+                 {:rgb "#143422", :name "Scoutgrün", :price (num->currency (rand 5))}
+                 {:rgb "#afc3d5", :name "Himmelfarben", :price (num->currency (rand 5))}
+                 {:rgb "#21145c", :name "Königsblau", :price (num->currency (rand 5))}
+                 {:rgb "#152442", :name "Marineblau", :price (num->currency (rand 5))}
+                 {:rgb "#f9f9f9", :name "Weiß", :price (num->currency (rand 5))}
+                 {:rgb "#7e7f84", :name "Grau", :price (num->currency (rand 5))}
+                 {:rgb "#000000", :name "Schwarz", :price (num->currency (rand 5))}
+                 {:rgb "#cd6888", :name "Pink", :price (num->currency (rand 5))}
+                 {:rgb "#b9cfe4", :name "Blaugrau", :price (num->currency (rand 5))}
+                 {:rgb "#44bce3", :name "Türkis", :price (num->currency (rand 5))}
+                 {:rgb "#ff5e00", :name "Orangerot", :price (num->currency (rand 5))}
+                 {:rgb "#c2bcec", :name "Fliederfarben", :price (num->currency (rand 5))}
+                 {:rgb "#e6ff15", :name "Tageslicht Gelb", :price (num->currency (rand 5))}]
+   :user  {:selected-color ""}})
 
-(defonce app-state
-         (atom
-           {:scarf {:color1 "white"
-                    :color2 "white"}
-            :user  {:selected-color ""}}))
-
-(defonce counter (atom 0))
-
-(defn get-unique-key
-  "Return unique react-key."
-  []
-  (str (:name config/project) "-unique-react-key-" (swap! counter inc)))
 
 ;;;; Auxiliary
-(defn get-selected-color
+#_(defn get-selected-color
   "Return the user's current selection."
   []
   (get-in @app-state [:user :selected-color]))
 
 
 ;;;; Functions concerning the reconciler
-(defmulti read (fn [env key params] key))
+(defn get-list [state key]
+  (let [st @state]
+    (mapv #(get-in st %) (get st key))))
 
+(defmulti read (fn [env key params] key))
 (defmethod read :default
   [{:keys [state] :as env} key params]
   (let [st @state]
@@ -65,22 +58,25 @@
       {:value value}
       {:value :not-found})))
 
-(defmulti mutate om/dispatch)
+(defmethod read :color/items
+  [{:keys [state]} key _]
+  {:value (get-list state key)})
 
+
+(defmulti mutate om/dispatch)
 (defmethod mutate 'color/temp
   [{:keys [state]} _ {:keys [color]}]
-  (println color)
   {:action (fn [] (swap! state update-in [:user :selected-color] (fn [] color)))})
 
-(defmethod mutate 'color/set
+#_(defmethod mutate 'color/set
   [{:keys [state]} _ {:keys [name field]}]
   (let [color (get-selected-color)]
     {:action (fn [] (swap! state update-in [:scarf field] (fn [] color)))}))
 
 (defonce reconciler
-         (om/reconciler
-           {:state  app-state
-            :parser (om/parser {:read read :mutate mutate})}))
+  (om/reconciler
+   {:state  app-state
+    :parser (om/parser {:read read :mutate mutate})}))
 
 
 ;;;; Colors
--- src/scarf/views.cljs
+++ src/scarf/views.cljs
@@ -12,25 +12,33 @@
 (def height "138px")
 
 (defui ColorBlock
+  static om/IQuery
+  (query [this]
+         [:rgb :name :price])
+  static om/Ident
+  (ident [this {:keys [rgb]}]
+         [:color/by-rgb rgb])
+
   Object
   (render [this]
           (let [color (om/props this)]
             (dom/div #js {:className "color-block-wrapper"
-                          :onClick   #(lib/save-selected-color! color)
+                          ;;:onClick   #(lib/save-selected-color! color)
                           :style     #js {:backgroundColor (:rgb color)}}
                      (dom/div #js {:className "color-block"})
                      (dom/span #js {:className "tooltiptext"} (:name color))))))
-(def color-block (om/factory ColorBlock {}))
+(def color-block (om/factory ColorBlock))
 
 (defui Colors
+  static om/IQuery
+  (query [this]
+         `[{:color/items ~(om/get-query ColorBlock)}])
   Object
   (render [this]
-          (dom/div nil
-                   (apply dom/ul #js {:id "colors"}
-                          (map #(color-block (merge {:react-key (rand-int 1000)} %)) lib/colors)))))
-(defn colors
-  "Create color palette"
-  [] (om/factory Colors {}))
+          (let [{:keys [color/items]} (om/props this)]
+            (dom/div nil
+                     (map color-block items)))))
+(def colors (om/factory Colors))
 
 (defui Scarf
   static om/IQuery
@@ -40,26 +48,26 @@
   (render [this]
           (dom/div nil
                    (dom/svg #js {:enableBackground "new 0 0 276 138"
-                                        ;:viewBow           "0 0 276 128"
+                                 ;; :viewBow           "0 0 276 128"
                                  :height            "138px"
                                  :width             "276px"
                                  :x                 "0"
                                  :y                 "0"}
                             (dom/g nil
-                                   (dom/polygon #js {:onClick #(lib/update-color! :color1)
-                                                     :fill    (:rgb (get-in @lib/app-state [:scarf :color1]))
+                                   (dom/polygon #js {;; :onClick #(lib/update-color! :color1)
+                                                     ;; :fill    (:rgb (get-in @lib/app-state [:scarf :color1]))
                                                      :points  "266.118,0 138.001,127.452 9.882,0 0,0 138.001,138 276,0"})
-                                   (dom/polygon #js {:onClick #(lib/update-color! :color2)
-                                                     :fill    (:rgb (get-in @lib/app-state [:scarf :color2]))
+                                   (dom/polygon #js {;; :onClick #(lib/update-color! :color2)
+                                                     ;; :fill    (:rgb (get-in @lib/app-state [:scarf :color2]))
                                                      :points  "266.118,0 246.666,0 138.001,108.833 29.333,0 9.882,0 138.001,127.452"})
-                                   (dom/polygon #js {:onClick #(lib/update-color! :color1)
-                                                     :fill    (:rgb (get-in @lib/app-state [:scarf :color1]))
+                                   (dom/polygon #js {;; :onClick #(lib/update-color! :color1)
+                                                     ;; :fill    (:rgb (get-in @lib/app-state [:scarf :color1]))
                                                      :points  "246.666,0 29.333,0 138.001,108.833"})))
                    (dom/div nil
-                            (dom/span nil "Current selection:")
-                            (dom/span #js {:id "current-selection"}
+                            #_(dom/span nil "Current selection:")
+                            #_(dom/span #js {:id "current-selection"}
                                       (color-block (lib/get-selected-color)))))))
-(def scarf (om/factory Scarf {}))
+(def scarf (om/factory Scarf))
 
 (defui Main
   Object
@@ -67,4 +75,4 @@
           (dom/div nil
                    (dom/h4 nil "scarf")
                    (scarf)
-                   (colors))))
+                   (colors (om/props this)))))
