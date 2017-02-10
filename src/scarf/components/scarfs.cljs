6f8f8ec92504201ed7c4129669d086369197f2ab
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-02-10 23:51:10 +0100

    Add second scarf; add list of scarfs

--- src/scarf/components/scarfs.cljs
+++ src/scarf/components/scarfs.cljs
@@ -3,31 +3,24 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
-(def width "280px")
-(def height "140px")
-(def scale 0.4)
+(def width 280)
+(def height 140)
 
+(defn- scale-dimensions [scale dimension]
+  (str (* scale dimension) "px"))
 
 (defn- colorize
   "Verify that a color has been selected before transacting a nil-value."
-  [this the-chosen-one field]
-  (when the-chosen-one
+  [this scale field]
+  (when (= 1 scale)
     (let [{:keys [color/selected]} (om/props this)]
       (when-not (nil? selected)
         (om/transact! this `[(scarf/colorize {:field ~field})])))))
 
-(defn- transform-current
-  "If this scarf is not the current activated scarf, reduce its size."
-  [the-chosen-one]
-  (if-not the-chosen-one
-    #js {:className "smooth"
-         :transform (str "scale(" scale ")")}
-    #js {:className "smooth"}))
-
 (defn- switch-chosen-one
   "If current scarf is not active but it has been clicked, make it the new chosen one."
-  [this id the-chosen-one]
-  (when-not the-chosen-one
+  [this id scale]
+  (when-not (= 1 scale)
     (om/transact! this `[(scarf/current {:id ~id})])))
 
 ;; -----------------------------------------------------------------------------
@@ -41,25 +34,23 @@
   Object
   (render [this]
           (let [id 1
-                {:keys [scarf/color1 scarf/color2 scarf/current]} (om/props this)
+                {:keys [scarf/color1 scarf/color2 scarf/current scale]} (om/props this)
+                scale (or scale 1)
                 c1 (:rgb color1)
                 c2 (:rgb color2)
-                the-chosen-one (= id current)]
-            (dom/svg #js {:id (str "scarf-" id)
-                          :enableBackground (str "new 0 0 " width " " height)
-                          :height height
-                          :width width
-                          :onClick #(switch-chosen-one this id the-chosen-one)
-                          :x "0"
-                          :y "0"}
-                     (dom/g (transform-current the-chosen-one)
-                            (dom/polygon #js {:onClick #(colorize this the-chosen-one :scarf/color2)
+                swidth (scale-dimensions scale width)
+                sheight (scale-dimensions scale height)]
+            (dom/svg #js {:onClick #(switch-chosen-one this id scale)
+                          :height sheight
+                          :width swidth}
+                     (dom/g #js {:transform (str "scale(" scale ")")}
+                            (dom/polygon #js {:onClick #(colorize this scale :scarf/color2)
                                               :fill c2
                                               :points  "266,0 247,0 138,109 29,0 10,0 138,127"})
-                            (dom/polygon #js {:onClick #(colorize this the-chosen-one :scarf/color1)
+                            (dom/polygon #js {:onClick #(colorize this scale :scarf/color1)
                                               :fill c1
                                               :points  "247,0 29,0 138,109"}))))))
-(def triangle-stripe (om/factory TriangleStripe {:foo :foo}))
+(def triangle-stripe (om/factory TriangleStripe))
 
 (defui TriangleStripeEdge
   "Dreieck mit dünnem Streifen und Randstück.
@@ -70,27 +61,36 @@
   Object
   (render [this]
           (let [id 0
-                {:keys [scarf/color1 scarf/color2 scarf/current]} (om/props this)
+                {:keys [scarf/color1 scarf/color2 scarf/current scale]} (om/props this)
+                scale (or scale 1)
                 c1 (:rgb color1)
                 c2 (:rgb color2)
-                the-chosen-one (= id current)]
-            (dom/svg #js {:id (str "scarf-" id)
-                          :enableBackground (str "new 0 0 " width " " height)
-                          :height height
-                          :width width
-                          :onClick #(switch-chosen-one this id the-chosen-one)
-                          :x "0"
-                          :y "0"}
-                     (dom/g (transform-current the-chosen-one)
-                            (dom/polygon #js {:onClick #(colorize this the-chosen-one :scarf/color1)
+                swidth (scale-dimensions scale width)
+                sheight (scale-dimensions scale height)]
+            (dom/svg #js {:onClick #(switch-chosen-one this id scale)
+                          :height sheight
+                          :width swidth}
+                     (dom/g #js {:transform (str "scale(" scale ")")}
+                            (dom/polygon #js {:onClick #(colorize this scale :scarf/color1)
                                               :fill c1
                                               :points  "266,0 138,127 10,0 0,0 138,138 276,0"})
-                            (dom/polygon #js {:onClick #(colorize this the-chosen-one :scarf/color2)
+                            (dom/polygon #js {:onClick #(colorize this scale :scarf/color2)
                                               :fill c2
                                               :points  "266,0 247,0 138,109 29,0 10,0 138,127"})
-                            (dom/polygon #js {:onClick #(colorize this the-chosen-one :scarf/color1)
+                            (dom/polygon #js {:onClick #(colorize this scale :scarf/color1)
                                               :fill c1
                                               :points  "247,0 29,0 138,109"}))))))
 (def triangle-stripe-edge (om/factory TriangleStripeEdge))
+
+;; -----------------------------------------------------------------------------
+
+(defui ListScarfs
+  Object
+  (render [this]
+          (let [scale 0.4]
+            (dom/div #js {:filter "gray"}
+                     (triangle-stripe-edge (merge (om/props this) {:scale scale}))
+                     (triangle-stripe (merge (om/props this) {:scale scale}))))))
+(def list-scarfs (om/factory ListScarfs))
 
 ;; (set! (.. (first (js/document.getElementsByTagName "g")) -style -transform) "scale(0.1)")
