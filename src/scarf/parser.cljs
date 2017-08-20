162d582d473275dcefa01d26ad6a815fadda6149
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-08-20 22:40:00 +0200

    Add new colors

--- src/scarf/parser.cljs
+++ src/scarf/parser.cljs
@@ -8,52 +8,55 @@
 (def init-data
   {:nav/category :einfach
    :scarf/current 5001
-   :scarf/mid1 {:id "10.00.82" :rgb "#dddddb" :name "hellgrau" :price (random-price 5)}
-   :scarf/mid2 {:id "10.00.69" :rgb "#2b613f" :name "dunkelgrün" :price (random-price 5)}
-   :scarf/stripe1 {:id "10.00.34" :rgb "#a91b29" :name "rot" :price (random-price 5)}
-   :scarf/stripe2 {:id "10.00.64" :rgb "#3f9d8d" :name "smaragd" :price (random-price 5)}
-   :scarf/stripe3 {:id "10.00.26" :rgb "#ffe880" :name "gelb" :price (random-price 5)}
-   :color/selected {:id "10.00.51" :rgb "#c2e2ed" :name "hellblau" :price (random-price 5)}
-   :color/items [{:id "10.00.24" :rgb "#f5eba4" :name "hellgelb" :price (random-price 5)}
-                 {:id "10.00.25" :rgb "#e14b30" :name "dunkelorange" :price (random-price 5)}
-                 {:id "10.00.26" :rgb "#ffe880" :name "gelb" :price (random-price 5)}
-                 {:id "10.00.27" :rgb "#e1a841" :name "dunkelgelb" :price (random-price 5)}
-                 {:id "10.00.28" :rgb "#eac7ab" :name "lachs" :price (random-price 5)}
-                 {:id "10.00.29" :rgb "#e25740" :name "orange" :price (random-price 5)}
-                 {:id "10.00.31" :rgb "#f2e8e9" :name "hellrosa" :price (random-price 5)}
-                 {:id "10.00.32" :rgb "#d98dbc" :name "rosa" :price (random-price 5)}
-                 {:id "10.00.33" :rgb "#b78489" :name "altrosa" :price (random-price 5)}
-                 {:id "10.00.34" :rgb "#a91b29" :name "rot" :price (random-price 5)}
-                 {:id "10.00.35" :rgb "#c54381" :name "pink" :price (random-price 5)}
-                 {:id "10.00.36" :rgb "#c62a74" :name "dunkelpink" :price (random-price 5)}
-                 {:id "10.00.38" :rgb "#8d1e2f" :name "weinrot" :price (random-price 5)}
-                 {:id "10.00.39" :rgb "#592728" :name "bordeaux" :price (random-price 5)}
-                 {:id "10.00.43" :rgb "#a795bd" :name "flieder" :price (random-price 5)}
-                 {:id "10.00.47" :rgb "#81629e" :name "lila" :price (random-price 5)}
-                 {:id "10.00.49" :rgb "#45335d" :name "dunkellila" :price (random-price 5)}
-                 {:id "10.00.51" :rgb "#c2e2ed" :name "hellblau" :price (random-price 5)}
-                 {:id "10.00.52" :rgb "#266ca8" :name "nato blau" :price (random-price 5)}
-                 {:id "10.00.53" :rgb "#70848b" :name "rauchblau" :price (random-price 5)}
-                 {:id "10.00.54" :rgb "#2b5492" :name "royal" :price (random-price 5)}
-                 {:id "10.00.59" :rgb "#283641" :name "marine" :price (random-price 5)}
-                 {:id "10.00.62" :rgb "#a2e071" :name "hellgrün" :price (random-price 5)}
-                 {:id "10.00.63" :rgb "#58b996" :name "türkis" :price (random-price 5)}
-                 {:id "10.00.64" :rgb "#3f9d8d" :name "smaragd" :price (random-price 5)}
-                 {:id "10.00.65" :rgb "#24764e" :name "grün" :price (random-price 5)}
-                 {:id "10.00.66" :rgb "#6b8353" :name "olive" :price (random-price 5)}
-                 {:id "10.00.68" :rgb "#264033" :name "tannengrün" :price (random-price 5)}
-                 {:id "10.00.69" :rgb "#2b613f" :name "dunkelgrün" :price (random-price 5)}
-                 {:id "10.00.73" :rgb "#9a9075" :name "sand" :price (random-price 5)}
-                 {:id "10.00.74" :rgb "#a63f36" :name "terrakotta" :price (random-price 5)}
-                 {:id "10.00.75" :rgb "#614a42" :name "milchkaffee" :price (random-price 5)}
-                 {:id "10.00.78" :rgb "#52352f" :name "rotbraun" :price (random-price 5)}
-                 {:id "10.00.79" :rgb "#41342c" :name "dunkelbraun" :price (random-price 5)}
-                 {:id "10.00.82" :rgb "#dddddb" :name "hellgrau" :price (random-price 5)}
-                 {:id "10.00.88" :rgb "#5f6460" :name "grau" :price (random-price 5)}
-                 {:id "10.00.89" :rgb "#2e3432" :name "schwarz" :price (random-price 5)}
-                 {:id "10.00.91" :rgb "#fbfcf6" :name "weiß" :price (random-price 5)}
-                 {:id "10.00.93" :rgb "#faf8ec" :name "natur" :price (random-price 5)}
-                 {:id "10.00.94" :rgb "#ebddb8" :name "beige" :price (random-price 5)}]})
+   :scarf/mid1 {:id "SW21" :rgb "#D0D6E0" :name "silbergrau" :price (random-price 5)}
+   :scarf/mid2 {:id "SW33" :rgb "#B1D3F9" :name "hellblau" :price (random-price 5)}
+   :scarf/stripe1 {:id "SW50" :rgb "#D14541" :name "hochrot" :price (random-price 5)}
+   :scarf/stripe2 {:id "SW47" :rgb "#48AE99" :name "smaragd" :price (random-price 5)}
+   :scarf/stripe3 {:id "SW30" :rgb "#659BCE" :name "marine" :price (random-price 5)}
+   :color/selected {:id "SW42" :rgb "#B0DE7C" :name "lemon" :price (random-price 5)}
+   :color/items [{:id "SW01" :rgb "#E2E2E2" :name "weiß" :price (random-price 5)}
+                 {:id "SW02" :rgb "#D1C6B7" :name "natur" :price (random-price 5)}
+                 {:id "SW10" :rgb "#F1D5B0" :name "beige" :price (random-price 5)}
+                 {:id "SW12" :rgb "#D7BC9F" :name "sand" :price (random-price 5)}
+                 {:id "SW14" :rgb "#D19B92" :name "hellbraun" :price (random-price 5)}
+                 {:id "SW15" :rgb "#968381" :name "dunkelbraun" :price (random-price 5)}
+                 {:id "SW20" :rgb "#4B4E51" :name "schwarz" :price (random-price 5)}
+                 {:id "SW21" :rgb "#D0D6E0" :name "silbergrau" :price (random-price 5)}
+                 {:id "SW22" :rgb "#BACFE4" :name "mittelgrau" :price (random-price 5)}
+                 {:id "SW23" :rgb "#9E9CA0" :name "dunkelgrau" :price (random-price 5)}
+                 {:id "SW30" :rgb "#659BCE" :name "marine" :price (random-price 5)}
+                 {:id "SW31" :rgb "#3F98EA" :name "royal" :price (random-price 5)}
+                 {:id "SW32" :rgb "#65D0FF" :name "türkis" :price (random-price 5)}
+                 {:id "SW33" :rgb "#B1D3F9" :name "hellblau" :price (random-price 5)}
+                 {:id "SW34" :rgb "#4894A7" :name "petrol" :price (random-price 5)}
+                 {:id "SW35" :rgb "#82A6E8" :name "flieder" :price (random-price 5)}
+                 {:id "SW36" :rgb "#8FA2E2" :name "lila" :price (random-price 5)}
+                 {:id "SW38" :rgb "#7ABBF5" :name "jeansblau" :price (random-price 5)}
+                 {:id "SW39" :rgb "#609DCA" :name "rauchblau" :price (random-price 5)}
+                 {:id "SW40" :rgb "#4EA57C" :name "gras" :price (random-price 5)}
+                 {:id "SW41" :rgb "#B7DAD6" :name "mint" :price (random-price 5)}
+                 {:id "SW42" :rgb "#B0DE7C" :name "lemon" :price (random-price 5)}
+                 {:id "SW43" :rgb "#90AF5A" :name "helloliv" :price (random-price 5)}
+                 {:id "SW44" :rgb "#81A68B" :name "kaki" :price (random-price 5)}
+                 {:id "SW45" :rgb "#7B7A65" :name "dunkeloliv" :price (random-price 5)}
+                 {:id "SW46" :rgb "#628E85" :name "tanne" :price (random-price 5)}
+                 {:id "SW47" :rgb "#48AE99" :name "smaragd" :price (random-price 5)}
+                 {:id "SW49" :rgb "#6AD07C" :name "giftgrün" :price (random-price 5)}
+                 {:id "SW50" :rgb "#D14541" :name "hochrot" :price (random-price 5)}
+                 {:id "SW52" :rgb "#D25756" :name "weinrot" :price (random-price 5)}
+                 {:id "SW53" :rgb "#9D4343" :name "bordeaux" :price (random-price 5)}
+                 {:id "SW55" :rgb "#DE8B99" :name "rosa" :price (random-price 5)}
+                 {:id "SWXX" :rgb "#E0C9D7" :name "hellrosa" :price (random-price 5)}
+                 {:id "SW56" :rgb "#D67E81" :name "altrosa" :price (random-price 5)}
+                 {:id "SW57" :rgb "#D95E7B" :name "pink" :price (random-price 5)}
+                 {:id "SW58" :rgb "#824455" :name "erika" :price (random-price 5)}
+                 {:id "SW60" :rgb "#DA6A42" :name "orange" :price (random-price 5)}
+                 {:id "SW62" :rgb "#DCA583" :name "lachs" :price (random-price 5)}
+                 {:id "SW63" :rgb "#DB7361" :name "hellterra" :price (random-price 5)}
+                 {:id "SW64" :rgb "#934240" :name "dunkelterra" :price (random-price 5)}
+                 {:id "SW70" :rgb "#DB9343" :name "dunkelgelb" :price (random-price 5)}
+                 {:id "SW71" :rgb "#DCC469" :name "sonnengelb" :price (random-price 5)}
+                 {:id "SW72" :rgb "#D0AF5F" :name "hellgelb" :price (random-price 5)}]})
 
 ;; -----------------------------------------------------------------------------
 ;; Parsing
