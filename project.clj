7e0088f4f55d24f7a3af3be4c157371e74e6c6ca
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-08-27 20:41:42 +0200

    Release 0.2.236

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scarf "0.2.226"
+(defproject scarf "0.2.236"
   :description "Create clickable SVG objects, which can be colorized"
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/clojurescript "1.9.908"]
