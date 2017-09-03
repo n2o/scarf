136c3d08840fa208d5ab1afc366a437465f505c5
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-09-03 17:19:40 +0200

    Remove console log

--- src/scarf/core.cljs
+++ src/scarf/core.cljs
@@ -1,10 +1,8 @@
 (ns scarf.core
   (:require [goog.dom :as gdom]
             [om.next :as om]
-            [scarf.lib :as lib]
             [scarf.parser :as p]
             [scarf.views :as views]))
-(enable-console-print!)
 
 (defonce reconciler
   (om/reconciler
